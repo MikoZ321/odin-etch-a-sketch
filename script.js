be8847d5896da5750dd898d92befc8a00015365b
MikoZ321 <0123456789miko@gmail.com>
2023-05-05 23:29:27 +0200

    Creat grid of pixels using DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+const gridSize = 16;
+
+const container = document.querySelector(".container");
+for (let i = 0; i < gridSize * gridSize; i++) {
+    const square = document.createElement('div');
+    square.classList.add("square");
+    container.appendChild(square);
+}
