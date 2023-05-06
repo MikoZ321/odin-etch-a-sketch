c664363a65083a3136b28b6091da59972fd9fbfa
MikoZ321 <0123456789miko@gmail.com>
2023-05-06 10:24:26 +0200

    Add button to select grid size square size not modified yet

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-const gridSize = 16;
-
 const container = document.querySelector(".container");
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const square = document.createElement('div');
-    square.classList.add("square");
-    container.appendChild(square);
+function makeGrid (gridSize) {
+    const grid = document.createElement('div');
+    grid.classList.add("grid");
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const square = document.createElement('div');
+        square.classList.add("square");
+        grid.appendChild(square);
+    }
+
+    container.appendChild(grid);
 }
+const sizeBtn = document.querySelector("button");
+
+sizeBtn.addEventListener('click', function (e) {
+    const grid = document.querySelector(".grid");
+
+    let gridSize = prompt("Enter grid side length:");
+
+    while (gridSize <= 0 || gridSize > 100) {
+        gridSize = prompt("Grid side length must be between 0 and 100");
+    }
+
+    makeGrid(gridSize);
+});
+makeGrid(16);
 
 const squares = document.querySelectorAll('.square');
 
